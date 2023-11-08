efca1aa09fa33b0f8b82b0cbb02704f0b626b665
nayonsoso <panda0329@naver.com>
2023-11-08 23:10:56 +0900

    feat: 커밋 테스트 진짜 final

--- src/main/java/com/example/demo/type/PenaltyCode.java
+++ src/main/java/com/example/demo/type/PenaltyCode.java
@@ -1,6 +1,6 @@
 package com.example.demo.type;
 
 public enum PenaltyCode {
-    //TODO : 패널티 코드를 정수로 나타내야 함 - enum 사용 예상
+    //TODO : 패널티 코드를 정수로 나타내야 함 - enum 사용 예상!!
     OFFENSE_CHAT, DELETE_MATCH_EVEN_SOMEONE_APPLIED, DELETE_MATCH_AFTER_CONFIRM;
 }
